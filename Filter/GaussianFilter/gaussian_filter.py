3a61db270a996c932da7685de39f4942277e0f39
ycl <529768926@qq.com>
2018-09-23 16:38:49 +0800

    finish gaussian gilter

--- Filter/GaussianFilter/gaussian_filter.py
+++ Filter/GaussianFilter/gaussian_filter.py
@@ -2,6 +2,7 @@
 
 from PIL import Image
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 # 1 dimension gaussian filter
@@ -40,13 +41,46 @@ def convovle_2d(origin, sigma):
         result[:, i] = convolve_1d(origin[:, i], sigma)
     return result.T
 
-def convocle_matrix2d(origin, sigma):
-    pass
 
+def convovle_matrix2d(origin, sigma):
+    origin = np.array(origin)
+    gaussian = [np.exp(-0.5*np.square(i/sigma)) for i in range(3)]
+    gaussian_matrix = np.array([[gaussian[2], gaussian[1], gaussian[2]],
+                       [gaussian[1], gaussian[0], gaussian[1]],
+                       [gaussian[2], gaussian[1], gaussian[2]]])
+    gaussian_matrix = gaussian_matrix / np.sum(gaussian_matrix)
+    result = np.zeros(origin.shape)
+    width = origin.shape[1]
+    height = origin.shape[0]
+    for x in range(height):
+        for y in range(width):
+            result[x, y] = gaussian_matrix[0, 0]*origin[max(x-1, 0), max(y-1, 0)] \
+                            + gaussian_matrix[0, 1]*origin[max(x-1, 0), y] \
+                            + gaussian_matrix[0, 2]*origin[max(x-1, 0), min(y+1, width-1)] \
+                            + gaussian_matrix[1, 0]*origin[x, max(y-1, 0)] \
+                            + gaussian_matrix[1, 1]*origin[x, y] \
+                            + gaussian_matrix[1, 2]*origin[x, min(y+1, width-1)] \
+                            + gaussian_matrix[2, 0]*origin[min(x+1, height-1), max(y-1, 0)] \
+                            + gaussian_matrix[2, 1]*origin[min(x+1, height-1), y] \
+                            + gaussian_matrix[2, 2]*origin[min(x+1, height-1), min(y+1, width-1)]
+    return result
 
-def gaussian_filter(origin_image):
-    origin_image = np.array(origin_image, dtype=np.float)
+def gaussian_filter():
+    a = [[1.0, 2.0, 3.0, 4.0, 5.0],
+         [1.0, 2.0, 3.0, 4.0, 5.0],
+         [1.0, 2.0, 3.0, 4.0, 5.0],
+         [1.0, 2.0, 3.0, 4.0, 5.0],
+         [1.0, 2.0, 3.0, 4.0, 5.0]]
+    # convolve 1d
+    print(convolve_1d(a, 0.8))
+    # convolve 2d with 2 1d
+    print(convovle_2d(a, 0.8))
+    # convolve 2d with mask
+    print(convovle_matrix2d(a, 0.8))
+    # filter image with 2dmask
+    image = np.array(Image.open("./lena.jpg"), dtype=np.float)
+    result = convovle_matrix2d(image, 0.8).astype(np.uint8)
+    plt.imshow(result)
+    plt.show()
 
-
-# print(convolve_1d([1.0, 2.0, 3.0, 4.0, 5.0], 1))
-print(convovle_2d([[1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 2.0, 3.0, 4.0, 5.0]], 1))
+gaussian_filter()
