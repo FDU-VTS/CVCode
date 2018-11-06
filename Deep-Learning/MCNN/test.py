43f56c40af7105844b2f80367e2b08c756e4ca48
ycl <529768926@qq.com>
2018-11-06 20:02:36 +0800

    MCNN 1.2

--- Deep-Learning/MCNN/test.py
+++ Deep-Learning/MCNN/test.py
@@ -10,27 +10,22 @@ import warnings
 import math
 import torch.nn as nn
 warnings.filterwarnings("ignore")
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def test():
-    print("data loading............")
-    test_data = shtu_dataset.ShanghaiTechDataset(mode="test")
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=False, num_workers=8)
-    print("init net............")
-    net = mcnn.MCNN().eval().to(DEVICE)
-    net.load_state_dict(torch.load("./model/mcnn.pkl"), strict=False)
+
+
+    net = mcnn.MCNN()
+    net.to("cuda")
+    net.load_staste_dict(torch.load("./model/mcnn.pkl"), strict=False)
+    net = net.to("cpu")
+
     print("start to predict...........")
     sum_mae = 0.0
     sum_mse = 0.0
-    for input, ground_truth in iter(test_loader):
-        input = input.float().to(DEVICE)
-        ground_truth = ground_truth.float().to(DEVICE)
-        output = net(input)
-        mae, mse = utils.get_test_loss(output, ground_truth)
-        sum_mae += float(mae)
-        sum_mse += float(mse)
-    print("best_mae:%.1f, best_mse:%.1f" % (sum_mae / len(test_loader), math.sqrt(sum_mse / len(test_loader))))
+    a = torch.zeros(1,1,1,1)
+    a = a.float()
+    net(a)
 
 
 if __name__ == "__main__":
