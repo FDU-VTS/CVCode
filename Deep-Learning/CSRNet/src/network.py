528620ba323fa04b4a6f2f5502964525a2223977
ycl <529768926@qq.com>
2018-11-07 23:08:47 +0800

    init CSRNet network

--- Deep-Learning/CSRNet/src/network.py
+++ Deep-Learning/CSRNet/src/network.py
@@ -4,3 +4,13 @@
 # 2018-11
 # ------------------------
 
+import torch.nn as nn
+
+
+class CSRNet(nn.Module):
+
+    def __init__(self):
+        super(CSRNet, self).__init__()
+
+    def forward(self):
+        pass
